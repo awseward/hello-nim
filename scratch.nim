f37d287a3bb362b2ade31badf7060ea7deff699e
Andrew Seward <awswrd@gmail.com>
2020-04-17 08:07:59 -0700

    section: The typedesc keyword

--- scratch.nim
+++ scratch.nim
@@ -1,11 +1,7 @@
-proc onlyIntOrString[T: int|string](x, y: T) = discard
+proc p(a: typedesc, b: a) = discard
 
-onlyIntOrString(123, 456)
-# onlyIntOrString(1.0, true) #type mismatch
-# onlyIntOrString("xy", 50) # invalid as 'T' cannot be both at the same time
+# compile
+p(int, 4)
 
-# Would need sthng like this
-proc hmm[T, U: int|string](x: T, y: U) = discard
-hmm 123, 456
-hmm "xy", 50
-hmm 420, "yolo"
+# no compile
+p(int, true)
