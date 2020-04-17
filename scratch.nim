4ddef95577f7fb7d65ce504283668eaa26c72a30
Andrew Seward <awswrd@gmail.com>
2020-04-17 09:58:04 -0700

    section: BindSym
    > [...] to use bound identifiers (aka symbols) instead of unbound
    > identifiers
    
    (I'm not 100% sure the implications of this)

--- scratch.nim
+++ scratch.nim
@@ -1,30 +1,17 @@
-# to work with Nim syntax trees, we need an API that is defined in the
-# ``macros`` module:
 import macros
 
-macro debug(args: varargs[untyped]): untyped =
-  # `args` is a collection of `NimNode` values that each contain the
-  # AST for an argument of the macro. A macro always has to
-  # return a `NimNode`. A node of kind `nnkStmtList` is suitable for
-  # this use case.
-  result = nnkStmtList.newTree()
-  # iterate over any argument that is passed to this macro:
-  for n in args:
-    # add a call to the statement list that writes the expression;
-    result.add newCall("write", newIdentNode("stdout"), newLit("[DEBUG] "))
-    # `toStrLit` converts an AST to its string representation:
-    result.add newCall("write", newIdentNode("stdout"), newLit(n.repr))
-    # add a call to the statement list that writes ": "
-    result.add newCall("write", newIdentNode("stdout"), newLit(": "))
-    # add a call to the statement list that writes the expressions value:
-    result.add newCall("writeLine", newIdentNode("stdout"), n)
+macro debug(n: varargs[typed]): untyped =
+  result = newNimNode(nnkStmtList, n)
+  for x in n:
+    add(result, newCall(bindSym"write", bindSym"stdout", newStrLitNode("[DEBUG] ")))
+    add(result, newCall(bindSym"write", bindSym"stdout", toStrlit(x)))
+    add(result, newCall(bindSym"write", bindSym"stdout", newStrLitNode(": ")))
+    add(result, newCall(bindSym"writeLine", bindSym"stdout", x))
 
 var
   a: array[0..10, int]
-  x  = "some string"
-  foo = (true, "junk", [2,34])
-
+  x = "some string"
 a[0] = 42
 a[1] = 45
 
-debug(a[0], a[1], x, foo)
+debug(a[0], a[1], x)
