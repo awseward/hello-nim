9af559d048fde6bcb8c4c74ad8f3491b73eca5a6
Andrew Seward <awswrd@gmail.com>
2020-04-17 10:38:07 -0700

    section: Pragmas, side effects

--- scratch.nim
+++ scratch.nim
@@ -1,7 +1,12 @@
-import lib/[
-  hello
-, goodbye
-]
+# This is a compiler error
+#
+# func f() =
+#   echo "test"
 
-llo()
-gbye()
+# This is not supposed to be, according to the tutorial, but it may just be out
+# of date.
+#
+# Error: invalid pragma: noSideEffect
+func f() =
+  {.noSideEffect.}
+  echo "test"
