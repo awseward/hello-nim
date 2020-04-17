278aca30cba6f3056fcd6be28585fc3fc62e4d43
Andrew Seward <awswrd@gmail.com>
2020-04-17 09:08:34 -0700

    section: Bind statement
    Still a bit at a loss, hasn't explained templates or modules yet so not
    100% clear on what this is actually about.

--- scratch.nim
+++ scratch.nim
@@ -1,6 +1,22 @@
-proc create*[T](): ref T =
-  # there is no overloaded 'init' here, so we need to state that it's an
-  # open symbol explicitly:
-  mixin init
-  new result
-  init result
+# The bind statement is the counterpart to the mixin statement. It can be used
+# to explicitly declare identifiers that should be bound early (i.e. the
+# identifiers should be looked up in the scope of the template/generic
+# definition):
+
+# module A
+var
+  lastId = 0
+
+template genId*: untyped =
+  bind lastId
+  inc lastId
+  lastId
+
+# module B
+import A
+
+echo genId()
+
+# But a bind is rarely useful because symbol binding from the definition scope is the default.
+
+# bind statements only make sense in templates and generics.
