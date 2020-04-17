72f8f2303b539770d5151fd32850fde1e2945f70
Andrew Seward <awswrd@gmail.com>
2020-04-17 10:05:14 -0700

    section: static[T]

--- scratch.nim
+++ scratch.nim
@@ -1,8 +1,17 @@
-# Style note: For code readability, it is the best idea to use the least
-# powerful programming construct that still suffices. So the "check list" is:
+import re
 
-# Use an ordinary proc/iterator, if possible.
-# Else: Use a generic proc/iterator, if possible.
-# Else: Use a template, if possible.
-# Else: Use a macro.
+proc precompiledRegex(pattern: static string): RegEx =
+  var res {.global.} = re(pattern)
+  return res
+
+
+const b = ["a", "b", "c"]
+
+var
+  a = ["foo", "bar", "baz"]
+  re1 = precompiledRegex("/d+")
+  re2 = precompiledRegex(b[0])
+  re3 = precompiledRegex(a[0])
+
+
 
