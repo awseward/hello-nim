32822de3255052914919e525180c52be2f02c211
Andrew Seward <awswrd@gmail.com>
2020-04-17 10:43:50 -0700

    Fun with pragmas

--- scratch.nim
+++ scratch.nim
@@ -1,12 +1,5 @@
-# This is a compiler error
-#
-# func f() =
-#   echo "test"
+proc `==`(x, y: ptr int): bool {.error.}
 
-# This is not supposed to be, according to the tutorial, but it may just be out
-# of date.
-#
-# Error: invalid pragma: noSideEffect
-func f() =
-  {.noSideEffect.}
-  echo "test"
+const foo = "ayyy"
+
+{.warning: "yup " & foo.}
