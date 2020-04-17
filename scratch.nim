3e0eb218419c7c1a7cafe41cd9ae527be8c0c94b
Andrew Seward <awswrd@gmail.com>
2020-04-17 07:34:20 -0700

    section: Defer
    Kind of like a `using` in C# or a `with` in Python

--- scratch.nim
+++ scratch.nim
@@ -1,4 +1,18 @@
-type
-  LoadError* = object of Exception
+proc main =
+  var f = open("numbers.txt")
+  defer: close(f)
+  f.write "abc"
+  f.write "123"
 
-raise newException(LoadError, "Failed to load data")
+# is rewritten to:
+
+proc mainRewritten =
+  var f = open("numbers.txt")
+  try:
+    f.write "xyz"
+    f.write "987"
+  finally: close(f)
+
+# main()
+mainRewritten()
+
