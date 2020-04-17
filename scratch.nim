e9d5672dd56f20df10da1e997b0562290a38efb5
Andrew Seward <awswrd@gmail.com>
2020-04-17 10:08:20 -0700

    section: Forcing compile time via static

--- scratch.nim
+++ scratch.nim
@@ -1,17 +1,3 @@
-import re
+import math
 
-proc precompiledRegex(pattern: static string): RegEx =
-  var res {.global.} = re(pattern)
-  return res
-
-
-const b = ["a", "b", "c"]
-
-var
-  a = ["foo", "bar", "baz"]
-  re1 = precompiledRegex("/d+")
-  re2 = precompiledRegex(b[0])
-  re3 = precompiledRegex(a[0])
-
-
-
+echo static(fac(5)), " ", static[bool](16.isPowerOfTwo)
