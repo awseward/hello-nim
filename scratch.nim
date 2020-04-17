0b0016557d741eed0fddddfa2cae112fd6343825
Andrew Seward <awswrd@gmail.com>
2020-04-17 06:46:59 -0700

    section: procCall

--- scratch.nim
+++ scratch.nim
@@ -1,27 +1,17 @@
 type
-  Expression = ref object of RootObj
-  Literal = ref object of Expression
+  Thing = ref object of RootObj
+  Unit = ref object of Thing
     x: int
-  PlusExpr = ref object of Expression
-    a, b: Expression
 
-method eval(e: Expression): int {.base.} =
-  raise newException(CatchableError, "Method without implementation override")
+method m(a: Thing) {.base.} =
+  echo "Thing"
 
-method eval(e: Literal): int = return e.x
+method m(a: Unit) =
+  # Call the base method:
+  procCall m(a.Thing)
+  echo "Unit: ", a.x
 
-method eval(e: PlusExpr): int =
-  # watch out: relies on dynamic binding
-  result = eval(e.a) + eval(e.b)
+let u = new(Unit)
+u.x = 19
 
-proc newLit(x: int): Literal =
-  new(result)
-  result.x = x
-
-proc newPlus(a, b: Expression): PlusExpr =
-  new(result)
-  result.a = a
-  result.b = b
-
-echo eval(newPlus(newPlus(1.newLit, 2.newLit), 4.newLit))
-echo eval 1.newLit.newPlus(2.newLit).newPlus(5.newLit)
+u.m
