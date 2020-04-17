acc6e9881d1368389673bc173487846137d4952b
Andrew Seward <awswrd@gmail.com>
2020-04-17 06:43:11 -0700

    section: Multimethods

--- scratch.nim
+++ scratch.nim
@@ -1,1 +1,27 @@
-# Arbitrary stuff here
+type
+  Expression = ref object of RootObj
+  Literal = ref object of Expression
+    x: int
+  PlusExpr = ref object of Expression
+    a, b: Expression
+
+method eval(e: Expression): int {.base.} =
+  raise newException(CatchableError, "Method without implementation override")
+
+method eval(e: Literal): int = return e.x
+
+method eval(e: PlusExpr): int =
+  # watch out: relies on dynamic binding
+  result = eval(e.a) + eval(e.b)
+
+proc newLit(x: int): Literal =
+  new(result)
+  result.x = x
+
+proc newPlus(a, b: Expression): PlusExpr =
+  new(result)
+  result.a = a
+  result.b = b
+
+echo eval(newPlus(newPlus(1.newLit, 2.newLit), 4.newLit))
+echo eval 1.newLit.newPlus(2.newLit).newPlus(5.newLit)
