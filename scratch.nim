f7a2c7abdb4faa390c228917b938ff08f16903a2
Andrew Seward <awswrd@gmail.com>
2020-04-17 09:38:28 -0700

    section: Identifier construction
    Woooo, more not 100% grasping

--- scratch.nim
+++ scratch.nim
@@ -1,40 +1,7 @@
-template declareInt(x: untyped) =
-  var x: int
+template typedef(name: untyped, typ: typedesc) =
+  type
+    `T name`* {.inject.} = typ
+    `P name`* {.inject.} = ref `T name`
 
-template declareInt2(x: typed) =
-  var x: int
-
-declareInt(a)
-a = 3
-
-# declareInt2(b) # invalid, because x has not been declared and so has no type
-
-template withFile(f, fn, mode, actions: untyped): untyped =
-  var f: File
-  if open(f, fn, mode):
-    try:
-      actions
-    finally:
-      close(f)
-  else:
-    quit("cannot open: " & fn)
-
-withFile(txt, "ttempl3.txt", fmWrite):
-  txt.writeLine("line 1")
-  txt.writeLine("line 2")
-
-# Usually to pass a block of code to a template the parameter that accepts the
-# block needs to be of type untyped. Because symbol lookups are then delayed
-# until template instantiation time:
-template t(body: typed) =
-  block:
-    echo "t!!"
-    body
-
-t:
-  var i = 1
-  echo i
-
-t:
-  var i = 2 # compile error (Error: redefinition of 'i'; previous declaration here: ...)
-  echo i
+typedef(myint, int)
+var x: PMyInt
